aa362a2190a21e1fad230a355a74be6bdb02b3fc
zengyu.zhan <zengyu.zhan@dji.com>
2019-10-23 14:48:47 +0800

    添加 Demo 项目结构

---
+++ app/src/main/java/com/zengyu/demo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zengyu.demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author zengyu.zhan
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
